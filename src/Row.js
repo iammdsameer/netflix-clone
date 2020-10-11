64339f01e3be2b61e368bb266560874f70b14bf3
Sameer Khan <itsmdsameerkhan@gmail.com>
2020-10-11 21:09:53 +0545

    done and dusted

--- src/Row.js
+++ src/Row.js
@@ -1,8 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import axios from './helpers/axios'
+import YouTube from 'react-youtube'
+import movieTrailer from 'movie-trailer'
 
-const Row = ({title, src}) => {
+import './Row.css'
+const imageSrc = 'https://image.tmdb.org/t/p/w500'
+const Row = ({title, src, featured}) => {
   const [movies, setMovies] = useState([])
+  const [trailerUrl, setTrailerUrl] = useState('')
   useEffect(() => {
     const getMovies = async () => {
       const response = await axios.get(src)
@@ -10,11 +15,35 @@ const Row = ({title, src}) => {
       // return response.data.results
     }
     getMovies()
-  }, [])
+  }, [src])
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 1
+    }
+  }
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl('')
+    } else {
+      movieTrailer(movie?.name || '')
+      .then(url => {
+        console.log(url)
+        const urlParams = new URLSearchParams(new URL(url).search)
+        console.log(urlParams.get('v'))
+        setTrailerUrl(urlParams.get('v'))
+      })
+      .catch(error => console.log(error))
+    }
+  }
   return (
-    <div>
-      <h1>{title}</h1>
-      {movies.map(movie => <img className='row__posters' src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.name} />)}
+    <div className='row'>
+      <h2>{title}</h2>
+      <div className='row__posters'>
+        {movies.map(movie => <img key={movie.id} onClick={() => handleClick(movie)} className={`row__poster ${featured && 'row__featured_poster'}`} src={`${imageSrc}${featured ? movie.poster_path: movie.backdrop_path}`} alt={movie.name} />)}
+      </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
--- src/helpers/endpoints.js
+++ src/helpers/endpoints.js
@@ -2,7 +2,13 @@ let API_KEY = '86c352242dc5a8b827d08c598a1e04ef'
 
 const urls = {
   getTrending: `/trending/all/week?api_key=${API_KEY}`,
-  getNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`
+  getNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  getTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  getActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  getComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  getHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  getRomanticMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  getDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
 }
 
 export default urls
